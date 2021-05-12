e293ce1fc84a8ae54ac6ac3cae29e3f6defdf95c
NotMyFault <mc.cache@web.de>
2021-05-12 02:51:01 +0200

    Closes #19

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("java")
     id("java-library")
     id("com.github.johnrengelman.shadow") version "7.0.0"
-    id("org.cadixdev.licenser") version "0.5.1"
+    id("org.cadixdev.licenser") version "0.6.0"
     id("org.ajoberstar.grgit") version "4.1.0"
 }
 
@@ -54,10 +54,10 @@ ext {
 version = String.format("%s-%s", rootVersion, buildNumber)
 
 configure<LicenseExtension> {
-    header = rootProject.file("HEADER")
+    header.set(resources.text.fromFile(file("HEADER.txt")))
     include("**/*.java")
     exclude("**/XMaterial.java")
-    newLine = false
+    newLine.set(false)
 }
 
 tasks.named<Copy>("processResources") {
